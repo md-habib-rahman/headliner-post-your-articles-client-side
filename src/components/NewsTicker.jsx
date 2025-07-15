1e13a556bfc2a2ce1426e7225368b5cfeaaabe37
Md. Habibur Rahman Bappy <habib.rahman@live.com>
2025-07-16 03:17:36 +0600

    extra section for home page(News Ticker)

---
+++ src/components/NewsTicker.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import Marquee from "react-fast-marquee";
+import { MdAnnouncement } from "react-icons/md";
+import axiosInstance from "../api/axiosInstance";
+import { FcNews } from "react-icons/fc";
+
+const NewsTicker = () => {
+  const {
+    data: headlines = [],
+    isLoading,
+    isError,
+  } = useQuery({
+    queryKey: ["tickerTexts"],
+    queryFn: async () => {
+      const res = await axiosInstance.get("/news/tickers");
+      return res.data;
+    },
+  });
+
+  return (
+    <div className="bg-yellow-100 border-b border-yellow-300 py-2 px-4 flex items-center gap-4">
+      <FcNews className=" text-xl animate-pulse" />
+      <Marquee pauseOnHover gradient={false} speed={50}>
+        {headlines.map((headline, index) => (
+          <span key={index} className="mx-6 font-medium text-gray-800">
+            📰 {headline}
+          </span>
+        ))}
+      </Marquee>
+    </div>
+  );
+};
+
+export default NewsTicker;
--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -3,11 +3,13 @@ import PaymentCards from "../components/PaymentCards";
 import HeroBanner from "../components/HeroBanner";
 import AllPublisher from "../components/AllPublisher";
 import Statistics from "../components/Statistics";
+import NewsTicker from "../components/NewsTicker";
 
 const Home = () => {
   return (
     <>
       <div className="max-w-7xl mx-auto">
+        <NewsTicker />
         <HeroBanner />
         <PaymentCards />
         <AllPublisher />
